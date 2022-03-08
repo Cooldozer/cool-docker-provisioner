d31c2397731ab5067074aff80f22c8c96c3a17df
Cool Dozer <cool@dozer.com>
2022-03-08 20:49:52 +0300

    Switch to compose multi-container mode

--- DeployShell.sh
+++ DeployShell.sh
@@ -15,7 +15,7 @@ service_name=${prefix}0demo${Sufix}
 image_name=target
 fake_image_name=fake
 
-ls
+
 az login --use-device-code
 
 if [ $(az group exists --name ${rg_name}) == true ]
@@ -26,19 +26,22 @@ fi
 az group create --name ${rg_name} --location ${location}
 
 az acr create --name ${reg_name} --resource-group ${rg_name} --sku standard --admin-enabled true
+
 pushd $(pwd)
 cd fake
-ls
 az acr build --file Dockerfile --registry ${reg_name} --image ${fake_image_name} .
 popd
+
 pushd $(pwd)
 cd target
-ls
 az acr build --file Dockerfile --registry ${reg_name} --image ${image_name} .
 popd
-ls
-sed -i 's/placeholder/${reg_name}/' ./docker-compose.yml
-ls
+
+sed -i "s/placeholder/${reg_name}/" ./docker-compose.yml
+
 az appservice plan create --name ${plan_name} --resource-group ${rg_name} --sku ${plan_size} --number-of-workers ${PlanWorkerNo} --is-linux
-ls
-az webapp create --resource-group ${rg_name} --plan ${plan_name} --name ${service_name} --multicontainer-config-type COMPOSE --multicontainer-config-file docker-compose.yml
+
+echo "Renew regestry password"
+password=$(az acr credential renew -n an9520regtempv --password-name password2 | jq .passwords[0].value)
+
+az webapp create --resource-group ${rg_name} --plan ${plan_name} --name ${service_name} --multicontainer-config-type COMPOSE --multicontainer-config-file docker-compose.yml --docker-registry-server-user ${reg_name} --docker-registry-server-password ${password}
