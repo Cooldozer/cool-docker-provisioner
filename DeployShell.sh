567b0ebe42808ae69a11b27e6ce169ffff2680c3
Cooldozer <100922687+Cooldozer@users.noreply.github.com>
2022-03-09 13:51:55 +0300

    Add support to not modified for web usage containers (#2)
    Co-authored-by: Cool Dozer <cool@dozer.com>

--- DeployShell.sh
+++ DeployShell.sh
@@ -4,7 +4,6 @@ PlanWorkerNo=$3
 DockerInstanceNo=$4
 location=$5
 
-appservice_no=${DockerInstanceNo};
 plan_size=${PlanSize}
 
 prefix=an${RANDOM:0:4}
@@ -12,7 +11,9 @@ rg_name=demo_${Sufix}
 reg_name=${prefix}0reg${Sufix}
 plan_name=${prefix}0demo_plan_${Sufix}
 service_name=${prefix}0demo${Sufix}
-image_name=${prefix}0demo${Sufix}
+image_name=target
+fake_image_name=fake
+
 
 az login --use-device-code
 
@@ -24,11 +25,25 @@ fi
 az group create --name ${rg_name} --location ${location}
 
 az acr create --name ${reg_name} --resource-group ${rg_name} --sku standard --admin-enabled true
+
+pushd $(pwd)
+cd fake
+az acr build --file Dockerfile --registry ${reg_name} --image ${fake_image_name} .
+popd
+
+pushd $(pwd)
+cd target
 az acr build --file Dockerfile --registry ${reg_name} --image ${image_name} .
+popd
+
+sed -i "s/reg_placeholder/${reg_name}/" ./docker-compose.yml
+sed -i "s/scale_placeholder/${DockerInstanceNo}/" ./docker-compose.yml
 
 az appservice plan create --name ${plan_name} --resource-group ${rg_name} --sku ${plan_size} --number-of-workers ${PlanWorkerNo} --is-linux
 
-for (( i=1; i <= ${appservice_no}; i++ ))
-do
-     az webapp create --resource-group ${rg_name} --plan ${plan_name} --name ${service_name}-${i} --deployment-container-image-name ${reg_name}.azurecr.io/${image_name}:latest
-done
+echo "Renew regestry password"
+password=$(az acr credential renew -n ${reg_name} --password-name password2 | jq -r .passwords[0].value)
+echo ${password}
+az webapp create --resource-group ${rg_name} --plan ${plan_name} --name ${service_name} --multicontainer-config-type COMPOSE --multicontainer-config-file docker-compose.yml --docker-registry-server-user ${reg_name} --docker-registry-server-password ${password}
+az webapp config appsettings set --name ${service_name} --resource-group ${rg_name} --settings DOCKER_REGISTRY_SERVER_USERNAME=${reg_name} DOCKER_REGISTRY_SERVER_PASSWORD=${password} DOCKER_REGISTRY_SERVER_URL=${reg_name}.azurecr.io
+az webapp restart --name ${service_name} --resource-group ${rg_name}
